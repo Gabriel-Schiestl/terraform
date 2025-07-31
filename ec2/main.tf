ec9399f6f2b9779276408639712f7f60db070eb9
Gabriel Felipe Kuhn Schiestl <gabriel.schiestl25@hotmail.com>
2025-07-30 22:09:00 -0300

    feat: implements variables

---
+++ ec2/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 6.0"
+    }
+  }
+
+  required_version = "1.12.2"
+}
+
+provider "aws" {
+  region = var.aws_region
+  profile = var.aws_profile
+}
+
+resource "aws_instance" "my_ec2" {
+  ami = var.instance_ami
+  
+  instance_type = var.instance_type
+
+  tags = var.instance_tags
+}
